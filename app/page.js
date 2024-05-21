6be8eb3c24fa68d1dce0b723df314549f86b644a
Rituraj Jha <riturajreal01@gmail.com>
2024-05-21 20:54:28 +0530

    added Header and Hero section

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Button>Subscribe</Button>
-      
-
+      <Hero/>
     </div>
   );
 }
