86a8a4846f32d813a418a2f5d9aeda0e5652e6cb
Rituraj Jha <riturajreal01@gmail.com>
2024-05-21 22:49:18 +0530

    completed clerk auth

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,13 +1,22 @@
+"use client"
 import { Button } from '@/components/ui/button'
+import { UserButton, useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
+  const {user, isSignedIn} = useUser();
   return (
     <div className='p-5 border-b shadow-sm'>
         <div className='flex items-center justify-between'>
             <Image src={'/logo.svg'} width={180} height={50} alt='logo' />
-            <Button>Get Started</Button>
+            { isSignedIn?
+            <div className='flex items-center gap-5'>
+              <Button variant='outline'>Dashboard</Button>
+              <UserButton/>
+            </div> : <Button>Get Started</Button>
+          }
+            
         </div>
     </div>
   )
