d8595432e71606fcae482b1145e3bc650c8fb8a3
Rituraj Jha <riturajreal01@gmail.com>
2024-05-23 16:04:45 +0530

    added form edit page

--- app/edit-form/[formId]/page.jsx
+++ app/edit-form/[formId]/page.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+"use client"
+import { db } from '@/configs'
+import { JsonForms } from '@/configs/schema'
+import { useUser } from '@clerk/nextjs'
+import { and, eq } from 'drizzle-orm'
+import { ArrowLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import React, { useState, useEffect } from 'react'
 
-const EditForm = () => {
+const EditForm = ({params}) => { // params return form ID
+
+  // current user
+  const {user} = useUser();
+
+  // Save Data Use which stores array
+  const [jsonForm, setJsonForm] = useState([]);
+
+  // to redirect on previous screen
+  const router = useRouter();
+
+  useEffect(()=> {
+    user&&GetFormData();
+  },[user]);
+
+  // get form data
+  const GetFormData = async()=> {
+
+    // select data from db based on id and user
+    const result = await db.select().from(JsonForms)
+    .where(and(eq(JsonForms.id , params?.formId), 
+    eq(JsonForms.createdBy ,user?.primaryEmailAddress?.emailAddress ) ));
+
+    // console.log(result);
+
+    // update jsonForm hook -- parse json
+    setJsonForm(JSON.parse(result[0].jsonform));
+
+    // console.log(JSON.parse(result[0].jsonform));
+    
+  }
+
   return (
-    <div>EditForm</div>
+    <div className='p-10'>
+  <h2 onClick={()=> router.back()} className='flex gap-2 items-center my-5 cursor-pointer hover:font-semibold transition-all'>
+    <ArrowLeft/> Back
+  </h2>
+    <div className='grid grid-cols-1 md:grid-cols-3'>
+
+    {/* Controller */}
+      <div>
+        Controller 
+      </div>
+
+    {/* Form */}
+      <div className='md:col-span-2'>
+      Form
+      </div>
+    </div>
+    
+
+    </div>
   )
 }
 
