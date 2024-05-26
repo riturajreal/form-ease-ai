1cdda7d52229aec80c98e321fa13f052eb4bb4bb
Rituraj Jha <riturajreal01@gmail.com>
2024-05-26 18:14:23 +0530

    update schema with Theme Options

--- configs/schema.js
+++ configs/schema.js
@@ -3,6 +3,9 @@ const { pgTable, serial, text, varchar } = require("drizzle-orm/pg-core");
 export const JsonForms = pgTable('jsonForms', {
     id:serial('id').primaryKey(),
     jsonform:text('jsonform').notNull(),
+    theme:varchar('theme'),
+    background:varchar('background'),
+    style:varchar('style'),
     createdBy:varchar('createdBy').notNull(),
     createdAt:varchar('createdAt').notNull()
 
