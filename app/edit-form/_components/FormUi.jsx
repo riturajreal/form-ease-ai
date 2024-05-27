517f2114ad966bdbae467671c9d145bf4b6b6237
Rituraj Jha <riturajreal01@gmail.com>
2024-05-27 21:58:21 +0530

    added functions to handdle chnages in the form fields

--- app/edit-form/_components/FormUi.jsx
+++ app/edit-form/_components/FormUi.jsx
@@ -20,9 +20,11 @@ const FormUi = ({ jsonForm, onFieldUpdate, deleteField, selectedTheme, selectedS
   const [formData, setFormData] = useState();
   
   
-  // handle input change
+  // handle Input Field change
   const handleInputChange= (event)=> {
-    const {name, value} =e.target;
+
+    // destrucing 
+    const {name, value} =event.target;
 
     setFormData({
       ...formData,
@@ -30,8 +32,34 @@ const FormUi = ({ jsonForm, onFieldUpdate, deleteField, selectedTheme, selectedS
     })
   }
 
+
+// handle Select and Radio Field Change
+const handleSelectChange=(name, value)=> {
+  setFormData({
+    ...formData,
+    [name]:value
+  })
+  
+}
+
+// handle Checkbox Change
+const handleCheckboxChange=(fieldName, itemName, value )=> {
+  console.log(fieldName, itemName, value);
+}
+
+
+
+
+  // onSubmit
+  const onFormSubmit=(event)=> {
+    event.preventDefault()
+    console.log(formData);
+  }
+
   return (
-    <div className="border rounded-md p-5 md:w-[600px]" data-theme={selectedTheme} 
+    <form 
+    onSubmit={onFormSubmit}
+    className="border rounded-md p-5 md:w-[600px]" data-theme={selectedTheme} 
     style={{
       boxShadow: selectedStyle?.key=='boxshadow'&& '5px 5px 0px black',
       border:selectedStyle?.key=='border'&&selectedStyle.value
@@ -48,7 +76,8 @@ const FormUi = ({ jsonForm, onFieldUpdate, deleteField, selectedTheme, selectedS
           {field?.fieldType == "select" ? (
             <div className="my-3 w-full">
               <label className="text-xs text-gray-500">{field.label}</label>
-              <Select>
+              <Select required = {field?.required} 
+              onValueChange={(v)=> handleSelectChange(field.fieldName,v)}>
                 <SelectTrigger className="w-full bg-transparent">
                   <SelectValue placeholder={field.placeholder} />
                 </SelectTrigger>
@@ -64,10 +93,12 @@ const FormUi = ({ jsonForm, onFieldUpdate, deleteField, selectedTheme, selectedS
           ) : field.fieldType == "radio" ? (
             <div className="my-3 w-full">
               <label className="text-xs text-gray-500">{field.label}</label>
-              <RadioGroup>
+              <RadioGroup required = {field?.required}> 
                 {field?.options?.map((item, index) => (
                   <div key={index} className="flex items-center space-x-4">
-                    <RadioGroupItem value={item.label} id={item.label} />
+                    <RadioGroupItem value={item.label} id={item.label}
+                    onClick={()=> handleSelectChange(field.fieldName, item.label)}
+                     />
                     <Label htmlFor={item.label}>{item.label}</Label>
                   </div>
                 ))}
@@ -79,13 +110,13 @@ const FormUi = ({ jsonForm, onFieldUpdate, deleteField, selectedTheme, selectedS
               {field?.options ? (
                 field?.options?.map((item, index) => (
                   <div key={index} className="flex gap-2 items-center">
-                    <Checkbox />
+                    <Checkbox onCheckedChange={(v)=> handleCheckboxChange(field?.label, item, v)} />
                     <h2>{item.label ? item.label : item}</h2>
                   </div>
                 ))
               ) : (
                 <div className="flex gap-2 items-center">
-                  <Checkbox />
+                  <Checkbox required={field.required} />
                   <h2>{field.label}</h2>
                 </div>
               )}
@@ -97,6 +128,8 @@ const FormUi = ({ jsonForm, onFieldUpdate, deleteField, selectedTheme, selectedS
                 type={field?.fieldType}
                 placeholder={field?.placeholder}
                 name={field?.fieldName}
+                required = {field?.required}
+                onChange={(e)=> handleInputChange(e)}
               />
             </div>
           )}
@@ -114,8 +147,9 @@ const FormUi = ({ jsonForm, onFieldUpdate, deleteField, selectedTheme, selectedS
       
       ))}
 
-    <button className="btn btn-primary">Submit</button>
-    </div>
+{/* SUBMIT BUTTOM */}
+    <button type='submit' className="btn btn-primary">Submit</button>
+    </form>
   );
 };
 
