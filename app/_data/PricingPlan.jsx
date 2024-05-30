8cdc853bd47c98047f56e4d7da4d59b7d88b14ed
Rituraj Jha <riturajreal01@gmail.com>
2024-05-30 09:21:39 +0530

    added pricing plan for upgradation using stripe

---
+++ app/_data/PricingPlan.jsx
@@ -0,0 +1,17 @@
+export default [
+    {
+        link:'https://buy.stripe.com/test_8wM3fIbTp0XMfDi9AB',
+        price :7.99,
+        priceId:'price_1PLz2TJztrrh3tAAIxqFKlsc',
+        duration:'Monthly'
+
+    },
+
+    {
+        link:'https://buy.stripe.com/test_fZeg2ug9FbCq0Io3ce',
+        price:49.00,
+        priceId:'price_1PLz1zJztrrh3tAAYDEt7KVa',
+        duration:'Yearly'
+
+    }
+]
