c16f93ae8d997e18ad2ce0a8149125fe46583ea5
Rituraj Jha <riturajreal01@gmail.com>
2024-05-31 09:20:07 +0530

    added middlware to route

--- middleware.js
+++ middleware.js
@@ -1,7 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-    
+    '/dashboard',
+    '/dashboard/responses',
+    '/dashboard/upgrade'
   ]);
 
   export default clerkMiddleware((auth, req) => {
